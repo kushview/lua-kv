7ac1c10e1097a45afa3a08f6137d20597fff7dd6
Michael Fisher <mfisher@kushview.net>
2020-12-23 06:04:51 -0500

    add object_userdata helper

--- kv/lua/object.hpp
+++ kv/lua/object.hpp
@@ -13,4 +13,10 @@ public:
     ~Object() = default;
 };
 
+template<typename T> static
+T* object_userdata (const sol::table& proxy) {
+    auto mt = proxy[sol::metatable_key];
+    return mt["__impl"].get_or<T*> (nullptr);
+}
+
 }}
