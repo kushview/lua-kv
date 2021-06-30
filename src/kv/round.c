7f5a4e0bad02537ad5077c9f1d47cd04361c1d6b
Michael R. Fisher <mfisher@kushview.net>
2021-06-30 16:47:35 -0400

    Add round to integer helper

--- src/kv/round.c
+++ src/kv/round.c
@@ -48,12 +48,34 @@ static int f_float (lua_State* L) {
     return 1;
 }
 
+static int f_integer (lua_State* L) {
+    switch (lua_type (L, 1)) {
+        case LUA_TNUMBER:
+            lua_pushinteger (L, (lua_Integer) lua_tonumber (L, 1));
+            break;
+        case LUA_TBOOLEAN:
+            lua_pushinteger (L, (lua_Integer) lua_toboolean (L, 1));
+            break;
+        default:
+            lua_pushinteger (L, 0);
+            break;
+    }
+    return 1;
+}
+
 static const luaL_Reg funcs[] = {
-    /// Round a number to 32bit precision
+    /// Round a number to 32bit precision.
     // @function float
     // @number input Number to round
     // @treturn number Rounded value
     { "float",  f_float },
+
+    /// Cast a number to an Integer.
+    // @function integer
+    // @number input Number to round
+    // @treturn int The converted integer
+    { "integer", f_integer },
+
     { NULL, NULL }
 };
 
